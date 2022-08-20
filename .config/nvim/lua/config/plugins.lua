b19c5a19099ec0eaba730f46c531b06d0ab78a65
Cla <clamattia@gmail.com>
2022-08-20 15:35:58 +0200

    renamed tmux conf

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -8,6 +8,7 @@ Plug 'nvim-telescope/telescope.nvim'
 Plug 'ThePrimeagen/harpoon'
 Plug 'nvim-treesitter/nvim-treesitter'
 Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/nvim-cmp'
 
 vim.call('plug#end')
 
