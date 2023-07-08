2c5b6ce96be6ecf872cd982491524b7a5496de4b
Cla <clamattia@gmail.com>
2023-07-08 10:51:43 +0200

    Remove unused and add keymap for TS

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -4,6 +4,9 @@ vim.keymap.set("n", "<leader>tt", vim.cmd.Ex)
 --undo-tree
 vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
 
+--ts-playground
+vim.keymap.set("n", "<leader>ts", vim.cmd.TSPlaygroundToggle)
+
 --telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -14,31 +17,6 @@ vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
 
 --tmux-sessionizer
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-
---line swapping
-vim.keymap.set("n", "<C-j>", ":m .+1<CR>==")
-vim.keymap.set("n", "<C-k>", ":m .-2<CR>==")
-vim.keymap.set("i", "<C-j>", "<Esc>:m .+1<CR>==gi")
-vim.keymap.set("i", "<C-k>", "<Esc>:m .-2<CR>==gi")
-vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
-
--- center cursor after search/scroll
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
--- paste/delete without overwriting anonymous buffer
-vim.keymap.set("x", "<leader>p", "\"_dP")
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
-
--- yank to system clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
-vim.keymap.set("v", "<leader>Y", "\"+Y")
 
 -- search and replace under cursor
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
